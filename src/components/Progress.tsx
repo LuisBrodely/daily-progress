9bdbf62cebad20023f0b14f1bfff3922d7e378cf
LuisBrodely <luisbrodely2@gmail.com>
2023-03-06 22:17:00 -0600

    Mejorando vista para moviles

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -7,10 +7,13 @@ const Progress = ({name,videos,colorPrimary,colorSecondary,watched}:IProgress) =
         <div style = {{marginBottom:'60px'}}>
             <h2>{name} ({videos} Videos)</h2>
             <p>Ingresa la cantidad de videos vistos:</p>
-            
-            <div style = {{...styles.container, backgroundColor: colorPrimary}}>
-                <div style = {{...styles.box,width:`${result.toFixed(1)}%`, backgroundColor: colorSecondary}} />
+
+            <div style = {{display:'flex',justifyContent:'center'}}>
+                <div style = {{...styles.container, backgroundColor: colorPrimary}}>
+                    <div style = {{...styles.box,width:`${result.toFixed(1)}%`, backgroundColor: colorSecondary}} />
+                </div>
             </div>
+            
             <p>{watched} videos equivalen a {result.toFixed(1)}% de los {videos} videos totales</p>
         </div>
     )
@@ -18,7 +21,7 @@ const Progress = ({name,videos,colorPrimary,colorSecondary,watched}:IProgress) =
 
 const styles = {
     container: {
-        width: '500px',
+        width: '90%',
         height: '30px'
     },
     box: {
